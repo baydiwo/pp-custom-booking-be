a3fb799fa846b13c023aba71efa5dcf4f34f2f22
Putra Pratama Nasution <putra@fameoapp.com>
2021-01-10 20:57:04 +0700

    testing redis

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,9 @@
 | and give it the Closure to call when that URI is requested.
 |
 */
+use Illuminate\Support\Facades\Redis;
 
-$router->get('/', function () use ($router) {
-    return $router->app->version();
+$router->get('/', function () {
+    $p = Redis::incr('p');
+    return $p;
 });
