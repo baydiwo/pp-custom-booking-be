fea0b57fc5f96769b8e7a490dea159a1c70432a7
Putra Pratama Nasution <putra@fameoapp.com>
2021-01-14 03:26:24 +0700

    register middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Middleware\Authenticate;
+
 require_once __DIR__.'/../vendor/autoload.php';
 
 (new Laravel\Lumen\Bootstrap\LoadEnvironmentVariables(
@@ -80,6 +82,11 @@ $app->configure('app');
 // $app->routeMiddleware([
 //     'auth' => App\Http\Middleware\Authenticate::class,
 // ]);
+
+$app->routeMiddleware([
+    'auth.token' => Authenticate::class,
+]);
+
 
 /*
 |--------------------------------------------------------------------------
