22cfd42be7fb1476ee1b3ea1596df95fa01bf90a
Putra Pratama Nasution <putra@fameoapp.com>
2021-01-30 23:07:24 +0700

    testing cors

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -72,8 +72,8 @@ class Authenticate
 
         $response = $next($request);
 
-        // $response->headers->set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
-        // $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With, Application','ip');
+        $response->headers->set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With, Application','ip');
 
         return $response;
     }
