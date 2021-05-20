e2db08612d437583be91ff14e31b83b682fd1664
sasikumarps <sasikumar@tendersoftware.in>
2021-05-20 15:41:11 +0530

    Update: updated the code to fetch the booking details based on bookingID

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rule;
 use App\Models\BookingDetails;
+use App\Models\PropertyAreaDetails;
 
 class BookingController
 {
@@ -101,7 +102,6 @@ class BookingController
 							'notes'      	=> $this->params['notes'],
 							'address'       => $this->params['address'],
 							'rateTypeId'  	=> $this->params['rateTypeId'],
-							//'resTypeId'  	=> $this->params['resTypeId'],
 							'state'         => $this->params['state'],
 							'town'          => $this->params['town'],
 							'countryId'    	=> $this->params['countryId'],
@@ -109,12 +109,10 @@ class BookingController
 							'phone'         => $this->params['phone'],
 							'postCode'     	=> $this->params['postCode'],
 							'pets'      	=> (isset($this->params['pets']) && $this->params['pets'] != '') ? $this->params['pets'] : 0,
-							'accomodation'  => $this->params['accomodationFee'],
-							'pet_fee'     	=> $this->params['petFee'],
-							'due_today'    	=> $this->params['dueToday'],
 							'guestId'		=> $guestId,
 							'bookingSourceId' => 200
 						];
+		$booking_id = rand(6,100000);
 
 		$model = new BookingDetails();
 		$model->arrival_date   	= $this->params['dateFrom'];
@@ -137,6 +135,9 @@ class BookingController
 		$model->phone         	= $this->params['phone'];
 		$model->post_code     	= $this->params['postCode'];
 		$model->pets      		= (isset($this->params['pets']) && $this->params['pets'] != '') ? $this->params['pets'] : 0;
+		$model->accomodation_fee= $this->params['accomodationFee'];
+		$model->pet_fee     	= $this->params['petFee'];
+		$model->due_today    	= $this->params['dueToday'];
 		$model->guest_id		= $guestId;
 		
         /*$endpoint = 'reservations?ignoreMandatoryFieldWarnings=true';
@@ -147,172 +148,126 @@ class BookingController
 
         if(isset($response['Message'])) {
             throw new Exception(ucwords($response['Message']));
-        }*/
+        }
 		
-		//$model->booking_id = (isset($response['id']) && $response['id'] != '') ? $response['id'] : 0;
-		$model->booking_id = rand(6,100000);
-		$response = array();//temporarily added when reserbvation API was blocked
+		$model->booking_id = (isset($response['id']) && $response['id'] != '') ? $response['id'] : 0;*/
+		
+		// Start - temporarily added when reserbvation API was blocked
+		$model->booking_id = $booking_id;
+		$response = array();
+		$response = [
+						"adults" => $this->params['adults'],
+						"bookingSourceName" => "",
+						"bookingSourceId" => 200,
+						"children" => $this->params['children'],
+						"companyId" => 0,
+						"discountId" => 0,
+						"groupAllotmentId" => 0,
+						"groupReservationId" => 0,
+						"infants" => $this->params['infants'],
+						"marketSegmentId" => 0,
+						"notes" => $this->params['notes'],
+						"otaNotes" => "",
+						"onlineConfirmationId" => 0,
+						"resTypeId" => 0,
+						"subMarketSegmentId" => 0,
+						"travelAgentId" => 0,
+						"userDefined1" => "",
+						"userDefined2" => "",
+						"userDefined3" => "",
+						"userDefined4" => "",
+						"userDefined5" => "",
+						"userDefined6" => "",
+						"userDefined7" => "",
+						"userDefined8" => "",
+						"userDefined9" => "",
+						"userDefined10" => "",
+						"userDefined11" => false,
+						"userDefined12" => false,
+						"userDefined13" => false,
+						"userDefined14" => "1900-01-01 00:00:00",
+						"userDefined15" => "1900-01-01 00:00:00",
+						"voucherId" => "",
+						"wholesalerId" => 0,
+						"id" => $booking_id,
+						"accountId" => 55262,
+						"areaId" => $this->params['areaId'],
+						"arrivalDate" => $this->params['dateFrom']." 15:00:00",
+						"cancelledDate" => "1900-01-01 00:00:00",
+						"categoryId" => $this->params['categoryId'],
+						"departureDate" => $this->params['dateTo']." 10:30:00",
+						"guestId" => 19439,
+						"rateTypeId" => $this->params['rateTypeId'],
+						"rateTypeName" => ($this->params['rateTypeId']+1)." Night OTA",
+						"status" => "Unconfirmed"
+					];
+		// End - temporarily added when reserbvation API was blocked
 		$model->save();
 		
         return [
             'code' => 1,
             'status' => 'success',
-            'data' => [],//$response->json(),
-			'postData' => $this->params
+            'data' => $response//->json() // temporarily commented, as the reserbvation API was blocked
         ];
     }
-
-    public function create_old()
+	
+    public function detail($id)
     {
         $api = new ApiController($this->authToken, $this->request);
-        $validator = Validator::make(
-            $this->params,
-            [
-                'arrivalDate'     => 'required|date_format:Y-m-d H:i:s',
-                'departureDate'   => 'required|date_format:Y-m-d H:i:s',
-                'surname'         => 'required',
-                'given'           => 'required',
-                'adults'          => 'required|integer',
-                'areaId'          => 'required|integer',
-                'categoryId'      => 'required|integer',
-                'children'        => 'required|integer',
-                'infants'         => 'required|integer',
-                'rateTypeId'      => 'required|integer',
-                'state'           => 'required',
-                'town'            => 'required',
-                'address'         => 'required',
-                'countryId'       => 'required|integer',
-                'nights'          => 'required|integer',
-                'email'           => 'required|email',
-                'phone'           => 'required',
-                'postCode'        => 'required',
-
-            ]
-        );
-
-        if ($validator->fails())
-            throw new Exception(ucwords(implode(' | ', $validator->errors()->all())));
-
-        $paramSearchGuest = [
-            "surname" => $this->params['surname'],
-            "given"   => $this->params['given'],
-            "mobile"  => $this->params['phone'],
-        ];
-
-        $searchGuest = $api->guestSearch($paramSearchGuest);
-        if((count($searchGuest) == 0) || (isset($searchGuest['Message']))) {
-            $paramCreateGuest = [
-                'addressLine1' => $this->params['address'],
-                'postCode'     => $this->params['postCode'],
-                'state'        => $this->params['state'],
-                'town'         => $this->params['town'],
-                'countryId'    => $this->params['countryId'],
-                'email'        => $this->params['email'],
-                'guestGiven'   => $this->params['given'],
-                'guestSurname' => $this->params['surname'],
-                'mobile'       => $this->params['phone'],
-            ];
-
-            $createGuest = $api->createGuest($paramCreateGuest);
-            if(isset($createGuest['Message'])) {
-                throw new Exception(ucwords($createGuest['Message']));
-            }
-            $this->params['guestId'] = $createGuest['id'];
-        } else {
-            $searchGuest = collect($searchGuest)->first();
-            $this->params['guestId'] = $searchGuest['id'];
-        }
-
-        $this->params['bookingSourceId'] = 200;
-
-        $endpoint = 'reservations?ignoreMandatoryFieldWarnings=true';
-
-        $response = Http::withHeaders([
-            'authtoken' => $this->authToken
-        ])->post(env('BASE_URL_RMS') . $endpoint, $this->params);
-
-        if(isset($response['Message'])) {
-            throw new Exception(ucwords($response['Message']));
-        }
+		$reservation = array();
+		$booking_details = BookingDetails::where('booking_id', $id)->first();
+		
+		$reservation['arrivalDate'] 	= $booking_details['arrival_date'];
+		$reservation['departureDate']	= $booking_details['departure_date'];
+		$reservation['adults']			= $booking_details['adults'];
+		$reservation['areaId']			= $booking_details['area_id'];
+		$reservation['categoryId']		= $booking_details['category_id'];
+		$reservation['children']		= $booking_details['children'];
+		$reservation['infants']			= $booking_details['infants'];
+		$reservation['notes']			= $booking_details['notes'];
+		$reservation['rateTypeId']		= $booking_details['rate_type_id'];
+		$reservation['nights']			= $booking_details['nights'];
+		$reservation['surname']			= $booking_details['surname'];
+		$reservation['given']			= $booking_details['given'];
+		$reservation['address']			= $booking_details['address'];
+		$reservation['email']			= $booking_details['email'];
+		$reservation['countryId']		= $booking_details['country_id'];
+		$reservation['postCode']		= $booking_details['post_code'];
+		$reservation['phone']			= $booking_details['phone'];
+		$reservation['state']			= $booking_details['state'];
+		$reservation['town']			= $booking_details['town'];
+		$reservation['accomodation']	= $booking_details['accomodation_fee'];
+		$reservation['petFee']			= $booking_details['pet_fee'];
+		$reservation['totalAmount']		= $booking_details['accomodation_fee'] + $booking_details['pet_fee'];
+		$reservation['dueToday']		= $booking_details['due_today'];
+		
+		$areaData = PropertyAreaDetails::where('area_id', $booking_details['area_id'])
+										->where('category_id', $booking_details['category_id'])
+										->first();
+		
+        $areaDetails['categoryId']      = $areaData['category_id'];
+        $areaDetails['areaName']    	= $areaData['name'];
+        $areaDetails['town']    	 	= $areaData['town'];
+        $areaDetails['description']     = $areaData['long_description'];
+        $areaDetails['imageUrl']     	= $areaData['image_link'];
+        $areaDetails['petAllowed']      = $areaData['pets_allowed'] == 0 ? false : true;
+        $areaDetails['petFee']          = $booking_details['pet_fee'];
+        $areaDetails['maxOccupants']    = (integer)$areaData['max_occupants'];
+        $areaDetails['totalRooms']      = (integer)$areaData['total_rooms'];
+        $areaDetails['totalGuests']     = $booking_details['adults'] . ' adults, ' . $booking_details['children'] . ' children, ' . $booking_details['infants'] . ' infants';
+        $areaDetails['totalBedrooms']   = (integer)$areaData['total_bedrooms'];
+        $areaDetails['totalBaths']      = (integer)$areaData['total_baths'];
+        $areaDetails['nights']          = $booking_details['nights'];
+        $areaDetails['accomodation']    = $booking_details['accomodation_fee'];
+        $areaDetails['totalAmount']     = $booking_details['accomodation_fee'] + $booking_details['pet_fee'];
+		$areaDetails['dueToday']        = $booking_details['due_today'];
+		
+		$data = ['reservationDetails' => $reservation, 'areaDetails' => $areaDetails];
+		
         return [
             'code' => 1,
             'status' => 'success',
-            'data' => $response->json()
-        ];
-    }
-
-    public function detail($id) 
-    {
-        $dataResponse = [];
-        $api = new ApiController($this->authToken, $this->request);
-        $detailReservation = $api->detailReservation($id);
-        if (isset($detailReservation['Message'])) {
-            throw new Exception('Data Reservation Not Found');
-        }
-
-        $detailGuest = $api->guestDetail($detailReservation['guestId']);
-        if (isset($detailGuest['Message'])) {
-            throw new Exception('Data Guest Not Found');
-        }
-        $dataGuest = collect($detailGuest)->first();
-        $dataGuest['propertyId'] = 1;
-        // if($dataGuest['propertyId'] == 0) {
-        //     throw new Exception('Property Guest Not Found');
-        // }
-
-        $detailSetting = $api->detailPropertySetting($dataGuest['propertyId']);
-        if (isset($detailSetting['Message'])) {
-            throw new Exception(ucwords($detailSetting['Message']));
-        }
-        $paramsRateQuote = [
-            'adults'        => $detailReservation['adults'],
-            'areaId'        => $detailReservation['areaId'],
-            'arrivalDate'   => $detailReservation['arrivalDate'],
-            'categoryId'    => $detailReservation['categoryId'],
-            'children'      => $detailReservation['children'],
-            'departureDate' => $detailReservation['departureDate'],
-            'infants'       => $detailReservation['infants'],
-            'propertyId'    => $dataGuest['propertyId'],
-            'rateTypeId'    => $detailReservation['rateTypeId'],
-        ];
-
-        $rateQuote = $api->rateQuote($paramsRateQuote);
-        if (isset($rateQuote['Message'])) {
-            throw new Exception(ucwords($rateQuote['Message']));
-        }
-
-
-        $arrivalDate   = Carbon::createFromFormat('Y-m-d H:s:i', $detailReservation['arrivalDate']);
-        $departureDate = Carbon::createFromFormat('Y-m-d H:s:i', $detailReservation['departureDate']);
-
-        $dataResponse['arrivalDate']   = $detailReservation['arrivalDate'];
-        $dataResponse['departureDate'] = $detailReservation['departureDate'];
-        $dataResponse['adults']        = $detailReservation['adults'];
-        $dataResponse['areaId']        = $detailReservation['areaId'];
-        $dataResponse['categoryId']    = $detailReservation['categoryId'];
-        $dataResponse['children']      = $detailReservation['children'];
-        $dataResponse['infants']       = $detailReservation['infants'];
-        $dataResponse['notes']         = $detailReservation['notes'];
-        $dataResponse['rateTypeId']    = $detailReservation['rateTypeId'];
-        $dataResponse['nights']        = $arrivalDate->diffInDays($departureDate);
-        $dataResponse['surname']       = $dataGuest['guestSurname'];
-        $dataResponse['given']         = $dataGuest['guestGiven'];
-        $dataResponse['address']       = $dataGuest['addressLine1'];
-        $dataResponse['email']         = $dataGuest['email'];
-        $dataResponse['countryId']     = $dataGuest['countryId'];
-        $dataResponse['postCode']      = $dataGuest['postCode'];
-        $dataResponse['phone']         = $dataGuest['mobile'];
-        $dataResponse['state']         = $dataGuest['state'];
-        $dataResponse['town']          = $dataGuest['town'];
-        $dataResponse['accomodation']  = collect($rateQuote['rateBreakdown'])->sum('totalRate');
-        $dataResponse['petFee']        = $detailSetting['petsAllowed'] == false ? 0 : 150;
-        $dataResponse['totalAmount']   = $dataResponse['accomodation'] + $dataResponse['petFee'];
-        $dataResponse['dueToday']      = $rateQuote['firstNightRate'];
-
-        return [
-            'code'   => 1,
-            'status' => 'success',
-            'data'   => $dataResponse
+            'data' => $data
         ];
     }
 }
