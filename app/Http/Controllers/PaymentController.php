0907fba94e8d19184fc239646137110e731166c4
sasikumarps <sasikumar@tendersoftware.in>
2021-11-17 12:43:08 +0530

    Update: Updated Payment session parameter - StoredCardIndicator

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -131,7 +131,7 @@ class PaymentController
             "currency"            => env('CURRENCY'),
             "merchantReference"   => "Acc No: ".$accountPropertyId,
             "storeCard"           => true,
-            "storeCardIndicator"  => "single",
+            "storedCardIndicator" => "installmentinitial",
             "callbackUrls" => [
                 "approved" => env('API_URL')."/transaction/success",
                 "declined" => env('API_URL')."/transaction/fail", 
