713318f8c662b5c92b4fff40e8dc37d3759fb7be
sasikumarps <sasikumar@tendersoftware.in>
2022-01-04 11:07:47 +0530

    Testing

--- public/index.php
+++ public/index.php
@@ -12,6 +12,8 @@ if ($method == "OPTIONS") {
 }
 
 ini_set('memory_limit','-1');
+ini_set('error_reporting','0');
+ini_set('display_errors','0');
 /*
 |--------------------------------------------------------------------------
 | Create The Application
