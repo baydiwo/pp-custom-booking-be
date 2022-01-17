d433ab54edfdce135cc012ed6d54caa1b486c00b
sasikumarps <sasikumar@tendersoftware.in>
2022-01-17 13:19:19 +0530

    Add: Checking RMS API execution timing for Pencil reservation

---
+++ app/Models/ModelTiming.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ModelTiming extends Model
+{
+    protected $table = 'tbl_timing';
+    public $timestamps = false;
+}
