0f1627906d4f8336300cf5f3f42b1f4b879c2426
sasikumarps <sasikumar@tendersoftware.in>
2022-03-01 15:25:18 +0530

    Revert "Revert "Change: updated db connection setup""
    This reverts commit ee409093af7bbd65a5559ff27f748c549ad2bfca.

--- config/database.php
+++ config/database.php
@@ -10,7 +10,7 @@ return [
     'default'     => 'db',
     'migrations'  => 'migrations',
     'connections' => [
-        'db' => [
+       /* 'db' => [
             'driver'    => env('DB_CONNECTION', 'mysql'),
             'host'      => env('DB_HOST', ''),
             'port'      => env('DB_PORT', ''),
@@ -25,7 +25,26 @@ return [
             'options'   => [
                 \PDO::ATTR_EMULATE_PREPARES => true
             ],
-        ],
+        ],*/
+		
+		 'db' => [
+			'driver' => 'mysql',
+			'host' => env('DB_HOST', ''),
+			'port' => env('DB_PORT', ''),
+			'database' => env('DB_DATABASE', ''),
+			'username' => env('DB_USERNAME', ''),
+			'password' => env('DB_PASSWORD', ''),
+			'unix_socket' => env('DB_SOCKET', ''),
+			'charset' => 'utf8mb4',
+			'collation' => 'utf8mb4_unicode_ci',
+			'prefix' => '',
+			'sslmode' => env('DB_SSLMODE', 'prefer'),
+			'options'   => array(
+				PDO::MYSQL_ATTR_SSL_CERT    => env('MYSQL_ATTR_SSL_CA', ''),
+			),
+			'strict' => true,
+			'engine' => null,
+		],
 
     ],
     'redis' => [
